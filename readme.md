fc8ae6659147b7b002178881e8bcfc3306f1f2a6
huffmanks <huffmanks2@gmail.com>
2024-05-13 20:01:51 -0400

    optional skip install on sh

--- readme.md
+++ readme.md
@@ -5,5 +5,15 @@ git clone https://github.com/huffmanks/config-stash.git
 ```
 
 ```sh
-chmod +x setup_dotfiles.sh && ./setup_dotfiles.sh
+chmod +x setup_dotfiles.sh
+```
+
+```sh
+# Skip installing packages
+./setup_dotfiles.sh --skip-install
+```
+
+```sh
+# Run entire script
+./setup_dotfiles.sh
 ```
--- setup_dotfiles.sh
+++ setup_dotfiles.sh
@@ -6,30 +6,35 @@ handle_error() {
     exit 1
 }
 
-# Check if the system is Linux
-if [[ $(uname) == "Linux" ]]; then
-    # Update package list and install necessary tools
-    sudo apt update && sudo apt upgrade -y || handle_error "Failed to update or upgrade packages"
-    sudo apt install -y git gh bat || handle_error "Failed to install git, gh and bat"
+# Check if the script should skip OS-specific installation
+if [[ "$1" == "--skip-install" ]]; then
+    echo "Skipping OS-specific installation..."
+else
+    # Check if the system is Linux
+    if [[ $(uname) == "Linux" ]]; then
+        # Update package list and install necessary tools
+        sudo apt update && sudo apt upgrade -y || handle_error "Failed to update or upgrade packages"
+        sudo apt install -y git gh bat || handle_error "Failed to install git, gh and bat"
 
-    # Install nvm and nodejs
-    curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash || handle_error "Failed to install nvm"
-    nvm install --lts || handle_error "Failed to install Node.js"
+        # Install nvm and nodejs
+        curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash || handle_error "Failed to install nvm"
+        nvm install --lts || handle_error "Failed to install Node.js"
 
-    # Install yarn and pnpm
-    npm i -g yarn pnpm || handle_error "Failed to install yarn and pnpm"
-fi
+        # Install yarn and pnpm
+        npm i -g yarn pnpm || handle_error "Failed to install yarn and pnpm"
+    fi
 
-# Check if the system is macOS
-if [[ $(uname) == "Darwin" ]]; then
-    # Install Homebrew
-    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)" || handle_error "Failed to install Homebrew"
+    # Check if the system is macOS
+    if [[ $(uname) == "Darwin" ]]; then
+        # Install Homebrew
+        /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)" || handle_error "Failed to install Homebrew"
 
-    # Install necessary tools using Homebrew
-    brew install gh bat node || handle_error "Failed to install gh, bat and node with Homebrew"
+        # Install necessary tools using Homebrew
+        brew install gh bat node || handle_error "Failed to install gh, bat and node with Homebrew"
 
-    # Install yarn and pnpm
-    npm i -g yarn pnpm || handle_error "Failed to install yarn and pnpm"
+        # Install yarn and pnpm
+        npm i -g yarn pnpm || handle_error "Failed to install yarn and pnpm"
+    fi
 fi
 
 # Setup zsh
